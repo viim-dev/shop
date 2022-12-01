14f6bffe305ea656fe4382464f58b96edde91f8f
toby <lloyd@emelle.me>
2022-12-01 14:38:53 -0500

    landing page update

---
+++ functions/shop/[[path]].js
@@ -0,0 +1,33 @@
+const LINK_TO = "https://viim.shop"
+export async function onRequestGet(context) {
+    
+    // Contents of context object
+    const {
+      request, // same as existing Worker API
+      env, // same as existing Worker API
+      params, // if filename includes [id] or [[path]]
+      waitUntil, // same as ctx.waitUntil in existing Worker API
+      next, // used for middleware or to fetch assets
+      data, // arbitrary space for passing data between middlewares
+    } = context;
+    const url = params.path?.join('/') ?? ''
+    // console.log(url)
+    return handleRequest(url);
+    
+    //try asset cdn
+    
+    // return env.ASSETS.fetch(request);
+    // return new Response();
+  }
+
+  async function handleRequest(district) {
+    const destinationURL = LINK_TO;
+    const statusCode = 301;
+    
+    try{
+        return Response.redirect(destinationURL, statusCode);
+    } catch(err){
+      console.log(err)
+    }
+    
+  }
