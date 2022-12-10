df17459beb679df042f2a270af91803b84ca30f8
bot <git@emelle.me>
2022-12-10 14:45:44 -0500

    updates

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ module.exports = (config) => {
   config.addPassthroughCopy({ 'public': './' })
   config.addPassthroughCopy({ 'src/admin/config.yml': './admin/config.yml' })
   config.addPassthroughCopy({"src/featured": './featured'});
-  config.addPassthroughCopy({"src/assets/uploads": './assets/uploads'});
+  config.addPassthroughCopy({"src/assets/uploads": 'assets/uploads'});
   config.addPassthroughCopy({ './src/assets': 'assets' }) 
   config.setBrowserSyncConfig({
     files: ['dist/**/*'],
